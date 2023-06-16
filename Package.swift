ca823b1229add9483171d7689c95612c52d878a0
Axel Ancona Esselmann <axel@anconaesselmann.com>
2023-06-16 11:32:51 -0700

    SPM

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
     name: "ThrottledTask",
     platforms: [
-           .macOS(.v10_15)
-       ]
+           .macOS(.v10_15),
+            .iOS(.v15)
+       ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
